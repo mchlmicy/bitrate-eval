5c8c07ece8ac1898e3badf0e015e4be042df38b8
Michael Young <mike@bitready.io>
2015-12-10 21:48:39 -0500

    Merged build/run as run, if you cant read source code; fuck you

--- build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo 'Installing requirements: pip, virtualenv'
-sudo easy_install pip
-sudo pip install virtualenv
-echo 'Building environment'
-virtualenv env
-source env/bin/activate
-pip install -r req*
--- run.sh
+++ run.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
+echo 'Installing requirements: pip, virtualenv'
+sudo easy_install pip
+sudo pip install virtualenv
+echo 'Building environment'
+virtualenv env
+source env/bin/activate
+pip install -r req*
 python main.py
